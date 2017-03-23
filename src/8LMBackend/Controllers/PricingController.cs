17c19e5be0f4b05740ecc222dd3bc8986f036274
Alexey <jurtbc@gmail.com>
2017-03-23 16:17:55 +0200

    added test pages

--- src/8LMBackend/Controllers/PricingController.cs
+++ src/8LMBackend/Controllers/PricingController.cs
@@ -3,9 +3,31 @@ using _8LMBackend.Service;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
+using System.IO;
+using System.Text;
+
 namespace _8LMCore.Controllers
 {
     public class PricingController : Controller
     {
+        [HttpPost]
+        public ActionResult ThankYouPage(){
+            var log = System.IO.File.CreateText("log.txt");
+            var bldr = new StringBuilder();
+            foreach(var key in Request.Form.Keys){
+                var val = Request.Form[key];
+                bldr.Append(key+ "=" + val);
+            }
+            log.Write(bldr.ToString());
+            log.Flush();
+            return Json(new {status = "ok"});
+        }
+
+        public ActionResult Buy(){
+            return View();
+        }
+
     }
+
+    
 }
